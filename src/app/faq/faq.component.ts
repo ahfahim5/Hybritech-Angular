65c302126c339dc5f8abc24e34b8c11739de4803
ahfahim5 <ahamzafahim@gmail.com>
2025-02-06 17:36:58 +0600

    Faq collapse issue fix

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './faq.component.css'
 })
 export class FAQComponent {
+  constructor() {}
 
+  collapseButton(event: MouseEvent) {
+    const button = event.target as HTMLElement;
+    const icon = button.querySelector("i");
+    if (icon) {
+      if (icon.classList.contains("fa-plus")) {
+        icon.className = "fa-solid fa-xmark text-white";
+      } else {
+        icon.className = "fa-solid fa-plus text-white";
+      }
+    }
+  }
 }
+
