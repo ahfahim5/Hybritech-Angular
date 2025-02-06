80b805935ed94f874b4236a92a1c02fd4e9d0c1a
ahfahim5 <ahamzafahim@gmail.com>
2025-02-06 15:21:00 +0600

    Career page Complete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { ServicesComponent } from './services/services.component';
 import { HomeComponent } from './home/home.component';
 import { CareerComponent } from './career/career.component';
+import {ContactUsComponent} from './contact-us/contact-us.component'
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'services', component: ServicesComponent },
     {path : 'about', component: AboutUsComponent},
-    {path : 'career', component: CareerComponent}
+    {path : 'career', component: CareerComponent},
+    {path : 'contact_us', component: ContactUsComponent}
 ];
